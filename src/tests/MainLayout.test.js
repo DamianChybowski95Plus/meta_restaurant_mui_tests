42c56600f399c628a0ce22259bd050137d897216
damianchybowski95 <141671141+damianchybowski95@users.noreply.github.com>
2023-11-15 19:10:43 +0100

    +

--- src/tests/MainLayout.test.js
+++ src/tests/MainLayout.test.js
@@ -1,12 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import MainLayout from 'views/MainLayout';
-import About from 'views/MainLayout/About';
-import Login from 'views/MainLayout/Login';
-import Menu from 'views/MainLayout/Menu';
-import Reservations from 'views/MainLayout/Reservations';
 
 test('Main Layout view tests', () => {
-  render(<MainLayout />)
   // expect(screen.getByText(/Littl/i)).toBeInTheDocument();
 });
 
